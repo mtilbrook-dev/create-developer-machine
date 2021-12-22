60af1526a19d56247e2e400567361be248612f43
Mitchell Tilbrook <marukami.kaito@gmail.com>
2021-12-22 12:23:29 +1100

    Update questions and finish 1Password key-gen

--- git-setup.sh
+++ git-setup.sh
@@ -1,42 +1,59 @@
 #!/usr/bin/env bash
 
+set -e
+
+email=""
+read -r -p 'Git Email? ' email
 sshPassword=""
 
 usePasswordManager="n"
-read -r -p 'Use password manager save SHH key password?: ' usePasswordManager
-if [ "$usePasswordManager" = "y" ]; then 
-  passwordManagerEmail="n"
-  read -r -p 'Password manager login email?: ' passwordManagerEmail
+read -r -p 'Use password manager save SHH key password? [y|n] ' usePasswordManager
+machineName=$(hostname)
+itemName="SSH-Key-$machineName"
+if [ "$usePasswordManager" = "y" ]; then
 
   useOnePassword="n"
-  read -r -p 'Use 1Password CLI?' useOnePassword
+  read -r -p 'Use 1Password CLI? [y|n] ' useOnePassword
   if [ "$useOnePassword" = "y" ]; then
     op --version || {
-      wget "https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_darwin_amd64_v1.8.0.pkg" "/tmp/one-password-cli.pkg"
+      wget "https://cache.agilebits.com/dist/1P/op/pkg/v1.12.3/op_apple_universal_v1.12.3.pkg" "/tmp/one-password-cli.pkg"
       sudo installer -pkg /tmp/one-password-cli.pkg
-    }
-    team=""
-    read -r -p '1Password Team?' team
-    op signin "$team" "$passwordManagerEmail"
-    # # OnePassword
-      # export OP_SESSION_tilbrook=""
-      # op-signin() {
-      #   eval $(op signin "$team")
-      # }
-  fi
 
-  useLastPassword="n"
-  read -r -p 'Use last password CLI?' useLastPassword
-  if [ "$useLastPassword" = "y" ]; then
-    lpass login "$passwordManagerEmail"
-    sshPassword=$(lpass generate --sync=auto --username=git-key --no-symbols "Github SSH" 16)
+      teamAddress="n"
+      read -r -p '1Password team address?: ' teamAddress
+      userAddress=""
+      read -r -p '1Password email address?' userAddress
+      op signin "$team"
+      op signin "$teamAddress" "$userAddress"
+    }
+    # Check for any CLI updates.
+    printf "\n\n"
+    op update
+
+    team=""
+    read -r -p '1Password team name? ' team
+    eval "$(op signin "$team")"
+
+    itemKey=$(
+      op create item login \
+        --title "$itemName" \
+        --generate-password='letters,digits,symbols,24' \
+        username="$email" | jq -r '.uuid'
+    )
+    sshPassword=$(op get item "$itemName" --fields password)
+    op get item "$itemKey" --fields password
   fi
-else 
-  read -r -p 'SHH key password?: ' usePasswordManager
+else
+  useLastPassword="n"
+  read -r -p 'Use last password CLI? [y|n] ' useLastPassword
+  if [ "$useLastPassword" = "y" ]; then
+    passwordManagerEmail="n"
+    read -r -p 'LastPassword login email? ' passwordManagerEmail
+    lpass login "$passwordManagerEmail"
+    sshPassword=$(lpass generate --sync=auto --username="$itemName" --no-symbols "Github SSH" 16)
+  fi
 fi
 
-email=""
-read -r -p 'Github Email?' email
 ssh-keygen -t rsa -b 4096 -C "$email" -P "$sshPassword" -f "$HOME/.ssh/id_rsa"
 
 eval "$(ssh-agent -s)"
@@ -57,11 +74,14 @@ ssh-add -K ~/.ssh/id_rsa
 echo "pbcopy < ~/.ssh/id_rsa.pub"
 pbcopy <~/.ssh/id_rsa.pub
 
-echo "Add the key to gtihub account"
+printf "\n\nAdd the key to gtihub account"
 echo "https://github.com/settings/ssh/new"
 
-githubUserName=""
-read -r -p 'Github user name?' githubUserName
-echo "Setting git config"
+gitUserName=""
+printf "\n\nSetting git config\n"
+read -r -p 'Git user Name? ' gitUserName
+read -r -p "Git user Email? $email" gitEmail
+gitEmail="${gitEmail:$email}"
+
 git config --global user.email "$email"
-git config --global user.name "$githubUserName"
+git config --global user.name "$gitUserName"
